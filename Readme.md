5c61c27761fb645a20bab2fca2bea3cd4fcce07f
Kristina Tymofiichuk <99436996+tymokri@users.noreply.github.com>
2022-07-01 18:05:02 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# JS-hw-2
+
+Creating tasks #1 #2 #3 (using variables in js)
---
+++ js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Task #1
+let name = 'John';
+let admin = 'John';
+
+alert(admin);
+
+// Task #2
+let planetName = 'Earth'
+let currentUser = 'Alex'
+
+// Task #3
+let sum = '10'+ 10;
+console.log(sum)
+
+// About task #3: result of sum string + number return just a string!
